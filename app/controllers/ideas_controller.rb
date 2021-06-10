191c02f6a722bc849684c1e8b7ebc84f546231c7
JuntOta <junto.ota@ezweb.ne.jp>
2021-06-10 19:54:44 +0900

    アソシエーション作成

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,2 +1,20 @@
 class IdeasController < ApplicationController
+
+  def index
+    @ideas = Idea.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+  
+  def show
+    @idea = Idea.find(params[:id])
+  end
+
+  private
+
+    def ideas
 end
